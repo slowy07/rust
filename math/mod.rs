180b0ea4e175a053b16da51ad9df1abdedf4fad4
slowy07 <slowy.arfy@gmail.com>
2022-02-07 11:13:48 +0700

    feat: adding prime check
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

--- math/mod.rs
+++ math/mod.rs
@@ -2,6 +2,7 @@ mod greatest_common_divisor;
 mod extended_euclidean;
 mod pascal_triangle;
 mod perfect_numbers;
+mod prime_check;
 
 pub use self::greatest_common_divisor::{
     greatest_common_divisor_iter, greatest_common_divisor_recursive
@@ -10,3 +11,5 @@ pub use self::greatest_common_divisor::{
 pub use self:extended_euclidean::extended_euclidean;
 pub use self::pascal_triangle::pascal_triangle;
 pub use self::perfect_numbers::perfect_number;
+pub use self::prime_check::prime_check;
+
