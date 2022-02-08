1ba3db5a1f69aa93bc44f40dbf89c31bd369169a
slowy07 <slowy.arfy@gmail.com>
2022-02-08 11:53:42 +0700

    feat: adding trial division

--- math/mod.rs
+++ math/mod.rs
@@ -3,6 +3,7 @@ mod extended_euclidean;
 mod pascal_triangle;
 mod perfect_numbers;
 mod prime_check;
+mod prime_check;
 
 pub use self::greatest_common_divisor::{
     greatest_common_divisor_iter, greatest_common_divisor_recursive
@@ -12,4 +13,5 @@ pub use self:extended_euclidean::extended_euclidean;
 pub use self::pascal_triangle::pascal_triangle;
 pub use self::perfect_numbers::perfect_number;
 pub use self::prime_check::prime_check;
+pub use self::trial_division::trial_division;
 
