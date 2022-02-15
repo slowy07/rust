0168321d3146616d80ba670325ec0d11c5e363de
slowy07 <slowy.arfy@gmail.com>
2022-02-15 22:57:45 +0700

    feat: adding depth first search
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

--- graph/mod.rs
+++ graph/mod.rs
@@ -1,5 +1,7 @@
 mod bellman_ford;
 mod breadth_first_search;
+mod depth_first_search;
 
 pub use self::bellman_ford::bellman_ford;
 pub use self::breadth_first_search::breadth_first_search;
+pub use self::depth_first_search::depth_first_search;
